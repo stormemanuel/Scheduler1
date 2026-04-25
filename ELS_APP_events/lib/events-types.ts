7e5034788a2d21c557de2b21d6f3800d741e3db1
stormemanuel <storm@emanuel-labor-services.com>
2026-04-25 14:28:29 -0400

    events

---
+++ ELS_APP_events/lib/events-types.ts
@@ -0,0 +1,29 @@
+export type ShowRecord = {
+  id: string;
+  name: string;
+  client: string;
+  venue: string;
+  rate_city: string;
+  show_start: string;
+  show_end: string;
+  notes: string;
+};
+
+export type LaborDayRecord = {
+  id: string;
+  show_id: string;
+  labor_date: string;
+  label: string;
+  notes: string;
+};
+
+export type SubCallRecord = {
+  id: string;
+  labor_day_id: string;
+  area: string;
+  role_name: string;
+  start_time: string;
+  end_time: string;
+  crew_needed: number;
+  notes: string;
+};
