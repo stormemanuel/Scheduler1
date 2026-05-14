dbadd7349ae21ba61f8d5f133e2e4ed14812ba4d
stormemanuel <storm@emanuel-labor-services.com>
2026-05-14 10:48:04 -0400

    Delete ELS18 directory

--- ELS18/lib/financial-types.ts
+++
@@ -1,6 +0,0 @@
-export type ShowFinancialRecord = {
-  show_id: string;
-  estimated_revenue_override: number | null;
-  expenses: number;
-  notes: string;
-};
--- ELS18/lib/rates-types.ts
+++
@@ -1,9 +0,0 @@
-export type MasterRateRecord = {
-  id: string;
-  city_name: string;
-  role_name: string;
-  full_day: number;
-  half_day: number | null;
-  overtime_multiplier: number;
-  doubletime_multiplier: number;
-};
